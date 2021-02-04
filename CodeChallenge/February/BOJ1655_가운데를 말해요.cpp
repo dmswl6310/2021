99703614eac846daa67d4d12658225dad4bd2d9e
황은지 <dmswl6310@gmail.com>
2021-02-04 17:30:12 +0900

    difficult

---
+++ CodeChallenge/February/BOJ1655_가운데를 말해요.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	priority_queue<int> bigger;
+	priority_queue<int> smaller;
+	
+	int N;
+	cin >> N;
+
+	int prev;
+	cin >> prev;
+
+	for (int i = 1; i < N; i++) {
+		int num;
+		cin >> num;
+		
+
+	}
+	return 0;
+}
